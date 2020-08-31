b782d6da998d32e44d26e151d758fc772eb185c9
Moonybb <moony_bb@naver.com>
2020-08-31 17:54:23 +0900

    struts01

--- frameWorkspace/struts01/src/com/bit/action/Ex01Action.java
+++ frameWorkspace/struts01/src/com/bit/action/Ex01Action.java
@@ -4,7 +4,7 @@ package com.bit.action;
 import com.opensymphony.xwork2.Action;
 
 public class Ex01Action implements Action {
-
+	
 	@Override
 	public String execute() throws Exception {
 		System.out.println("스트럿츠로 화면을 뿌리겠습니다.");
---
+++ frameWorkspace/struts01/src/com/bit/action/Ex11Action.java
@@ -0,0 +1,32 @@
+package com.bit.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class Ex11Action implements Action {
+
+	private String id;
+	private int num;
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public int getNum() {
+		return num;
+	}
+	
+	@Override
+	public String execute() throws Exception {
+		System.out.println("num::"+num+", id::"+id);
+		return Action.SUCCESS;
+	}
+
+}
